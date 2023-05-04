38d1e10a42fc322c5ab5d37cfe99b7b8b9691125
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-04 21:36:23 +0300

    completed lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def show
+    user = User.find(params[:id])
+    render json: user, only: [:id, :username, :city], include: {
+      items: {
+        except: [:created_at, :updated_at,:user_id]
+      }
+    }
+  end
 end
